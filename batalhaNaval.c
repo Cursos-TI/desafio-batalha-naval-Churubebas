2a00635a694093fda5140f17b13ad1262481bfb5
Hiago Ioris <hiagoioris@gmail.com>
2025-04-08 14:01:12 +0000

    Desafio Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,11 +5,41 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[11][11]={{1,0,0,0,0,0,0,0,0,0,0},{2,0,0,0,0,0,0,0,0,0,0},{3,0,0,0,0,0,0,0,0,0,0},{4,0,0,0,0,0,0,0,0,0,0},{5,0,0,0,0,0,0,0,0,0,0},{6,0,0,0,0,0,0,0,0,0,0},{7,0,0,0,0,0,0,0,0,0,0},{8,0,0,0,0,0,0,0,0,0,0},{9,0,0,0,0,0,0,0,0,0,0},{10,0,0,0,0,0,0,0,0,0,0}};
+    int navio1[3]={3,3,3}, navio2[3]={3,3,3};
+    int linha,coluna,x,y;
 
+        printf("\tBem Vindo a Batalha Naval!\n");
+        printf("\tImpressão do Tabuleiro\n");
+        printf("    A  B  C  D  F  G  H  I  J  L\n");
+
+         for (linha = 0; linha < 10; linha++)
+            {
+               for (coluna = 0; coluna < 11; coluna++)
+               {
+                printf(" %d ", tabuleiro[linha][coluna]);
+               }
+               printf("\n");
+            }
+                for (x = 2; x < 5; x++)
+                {
+                    tabuleiro[3][x]=3;
+                }
+                for (y = 5; y < 8; y++)
+                {
+                    tabuleiro[y][8]=3;
+                }
+                printf("\tImpressão dos Navios!\n");
+        printf("    A  B  C  D  F  G  H  I  J  L\n");
+
+         for (linha = 0; linha < 10; linha++)
+            {
+               for (coluna = 0; coluna < 11; coluna++)
+               {
+                printf(" %d ", tabuleiro[linha][coluna]);
+               }
+               printf("\n");
+            }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
